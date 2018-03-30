248dfd13d089b32df6478b37b0fabdbe4c6e152b
Ricardo Ledo <ledo.tulio@gmail.com>
2018-03-30 22:03:54 +0100

    php: update php 7.2 contaianer image
    Use my personal php 7.2 image instead of official, because the official
    have no somethings like git.

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 docker pull php:7.1-cli-alpine
-docker pull php:7.2-cli-alpine
 
 docker pull prooph/composer:7.1
 docker pull prooph/composer:7.2
 
 docker pull ricardotulio/php:5.6.32-cli
+docker pull ricardotulio/php:7.2-cli
 docker pull ricardotulio/composer:php-5.6.32
 docker pull ricardotulio/laravel-installer
 docker pull ricardotulio/lumen-installer
@@ -15,10 +15,10 @@ docker pull ricardotulio/phpmd
 
 docker tag ricardotulio/php:5.6.32-cli php-local-images:5.6.32
 docker tag ricardotulio/php:5.6.32-cli php-local-images:5.6
+docker tag ricardotulio/php:7.2-cli php-local-images:7.2
+docker tag ricardotulio/php:7.2-cli php-local-images:7
 
 docker tag php:7.1-cli-alpine php-local-images:7.1
-docker tag php:7.2-cli-alpine php-local-images:7.2
-docker tag php:7.2-cli-alpine php-local-images:7
 
 docker tag ricardotulio/composer:php-5.6.32 composer-local-images:php-5.6.32
 docker tag ricardotulio/composer:php-5.6.32 composer-local-images:php-5.6
